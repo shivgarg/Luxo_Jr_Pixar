2d38c21b96796d48839b9d6050a726143a65b9dd
Shivam Garg <this.is.shivgarg@gmail.com>
2014-10-26 22:26:53 +0530

    first

--- a.cpp
+++ a.cpp
@@ -12,10 +12,51 @@
 
 void display()
 {
+	glMatrixMode(GL_MODELVIEW);
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Load Identity
+	glLoadIdentity();
+	// Base Transformation
+	glRotatef(45,1,0,0);
+
+	glTranslatef(5,5,0);
+	
+	// Draw base;
+	glBegin(GL_POINTS);
+	for(int i=0;i<1000;++i)
+	  {
+	  	glVertex3f(cos(5*3.14159*i/1000.0),0,sin(5*3.14159*i/1000.0));
+	  }
+ 	glEnd();
+ 	
+ 	glRotatef(30,0,1,0);
+ 	glScalef(1,5,1);
+ 	glTranslatef(0,0.5,0);
+	glutWireCube(1.0);
+
+	//glScalef(1,5,1);
+	glTranslatef(0,0.75,0);
+	glutWireCube(0.5);
+
+
+	
+		
+	glFlush();
+	glutSwapBuffers();
+
 
 }
 
+void reshape(int x,int y)
+{
+    if (y == 0 || x == 0) return;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-10.0, 10.0, -10.0, 10.0, -100.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0,0,x,y);
 
+}
 int main (int argc, char **argv)
 {
     
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	g++ a.cpp -lGL -lGLU -lglut -O2 -o a
