060fe6d15a85a1870823dc847bedb00dd3c89ef6
Shivam Garg <bhatiyari.cse.iitd.ac.in>
2014-10-28 19:09:57 +0530

    changed coordinate system

--- a.cpp
+++ a.cpp
@@ -14,12 +14,11 @@
 using namespace std;
 
 
-double x,z;
+
 vector3d vball;
 int refreshMills=10;
-double phi=0,theta=0;
-double cube1=0.0,cube2=0.0,floorsize=0.0;
-
+double x,z,phi,theta,alpha,floorsize,baserad,baseht,c1rad,c2rad,c1ht,c2ht,bulbr1,bulbr2,bulbht,t=0;;
+GLUquadricObj *base,*btm,*top,*bulb,*cube1,*cube2;
 
 void keySpecialUp(int key, int x, int y) {
   switch (key) {
@@ -27,6 +26,8 @@ void keySpecialUp(int key, int x, int y) {
     case GLUT_KEY_DOWN: phi-=10; break;
     case GLUT_KEY_LEFT: theta-=10; break;
     case GLUT_KEY_RIGHT: theta+=10; break;
+    case GLUT_KEY_F5:  alpha+=10; break;
+    case GLUT_KEY_F6: alpha-=10; break;
   }
 }
 
@@ -36,65 +37,66 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0,10,-20,0,0,0,0,1,0);
+	gluLookAt(0,-20,t,0,0,0,0,0,1);
 	
 	glColor3f(1.0,0,0);
-	glBegin(GL_QUADS);
-		glVertex3f(-floorsize,0,-floorsize);
-		glVertex3f(-floorsize,0,floorsize);
-		glVertex3f(floorsize,0,floorsize);
-		glVertex3f(floorsize,0,-floorsize);
-	glEnd();
+	// glBegin(GL_QUADS);
+	// 	glVertex3f(-floorsize,-floorsize,0);
+	// 	glVertex3f(-floorsize,floorsize,0);
+	// 	glVertex3f(floorsize,floorsize,0);
+	// 	glVertex3f(floorsize,-floorsize,0);
+	// glEnd();
 	glColor3f(1,1,1);
 	
+	
+	gluCylinder(base,baserad,baserad,baseht,20,100);
 	glPushMatrix();
-	GLUquadricObj *base,*btm,*top,*bulb;
-	base=gluNewQuadric();
-	btm=gluNewQuadric();
-	top=gluNewQuadric();
-	bulb=gluNewQuadric();
-	glRotatef(90,1,0,0);
-	gluCylinder(base,1,1,0.15,20,100);
+	//glTranslatef(0,0,-1*baseht/2.0);
 	glColor3f(0,0,1);
-	gluDisk(btm,0,1,100,100);
-
-	glRotatef(-90,1,0,0);
-	glTranslatef(0,0,0.15);
-	glColor3f(0,1,0);
-	gluDisk(btm,0,1,100,100);
-	glColor3f(1,1,1);
- 	
- 	//glScalef(1,5,1);
- 	glRotatef(phi,0,0,-1);
- 	glTranslatef(0,cube1/2.0,0);
-	glutWireCube(cube1);
-
-	
-	glTranslatef(0,cube1/2.0,0);
-	glRotatef(theta,0,0,-1);
-	glTranslatef(0,cube2/2.0,0);
-	glutWireCube(cube2);
-	//glScalef(1,0.2,1);
-
-	glRotatef(0,0,0,-1);
-	glTranslatef(0,0.1+cube2/2.0,0);
-	glRotatef(90,1,0,0);
-	gluCylinder(bulb,0.3,0.1,0.2,50,50);
-	glRotatef(-90,1,0,0);
-	
-
+	gluDisk(btm,0,baserad,100,100);
 	glPopMatrix();
-	gluLookAt(0,10,-20,0,0,0,0,1,0);
-	glTranslatef(x,0,z);
-	cout << "x "<<x<<" z "<< z << endl;
-	glutSolidSphere(1.0,500,500);
-	x+=vball.x*refreshMills/1000;
-	z+=vball.z*refreshMills/1000;
-	if(abs((int)x)>(int)floorsize)
-		x=-x;
-	if(abs((int)z)>(int)floorsize)
-		z=-z;
-	//usleep(10000);
+	glPushMatrix();
+	glTranslatef(0,0,baseht);
+	glColor3f(0.5,0.5,0.5);
+	gluDisk(top,0,baserad,100,100);
+	glPopMatrix();
+	glTranslatef(0,0,baseht);
+	glColor3f(1,0.5,0.5);
+ 	
+ 	glRotatef(phi,0,1,0);
+ 	//glTranslatef(0,0,c1ht/2.0);
+ 	//glRotatef(90,1,0,0);
+	gluCylinder(cube1,c1rad,c1rad,c1ht,20,100);
+	//glRotatef(-90,1,0,0);
+	
+	 glTranslatef(0,0,c1ht);
+	 glRotatef(theta,0,1,0);
+	// glTranslatef(0,0,c2ht/2.0);
+
+	// //glRotatef(90,1,0,0);
+	 gluCylinder(cube2,c2rad,c2rad,c2ht,20,100);
+	// //glRotatef(-90,1,0,0);
+	
+	 
+	 glTranslatef(0,0,c2ht);
+	 glRotatef(alpha,0,1,0);
+	// //glRotatef(90,1,0,0);
+	 gluCylinder(bulb,bulbr1,bulbr2,bulbht,50,50);
+	// //glRotatef(-90,1,0,0);
+	
+
+	// glPopMatrix();
+	// gluLookAt(0,-20,0,0,0,0,0,0,1);
+	// glTranslatef(x,z,0);
+	// cout << "x "<<x<<" z "<< z << endl;
+	// glutSolidSphere(1.0,500,500);
+	// x+=vball.x*refreshMills/1000;
+	// z+=vball.z*refreshMills/1000;
+	// if(abs((int)x)>(int)floorsize)
+	// 	x=-x;
+	// if(abs((int)z)>(int)floorsize)
+	// 	z=-z;
+	// //usleep(10000);
 
 	
 		
@@ -138,15 +140,29 @@ void timer(int value) {
 
 void init()
 {
+	 baserad=0.6;
+	 baseht=0.15;
+	 c1rad=0.3;
+	 c2rad=0.2;
+	 c1ht=1.2;
+	 c2ht=0.8;
+	 bulbr1=0.2;
+	 bulbr2=0.4;
+	 bulbht=0.3;
 	 x=0;z=0;
 	 vball.x=30;
 	 vball.y=0;
 	 vball.z=10;
-	 cube1=0.8;
-	 cube2=0.4;
 	 floorsize=50;
 	 phi=0;
-	 theta=0;
+	 theta=30;
+	 alpha=40;
+	 base=gluNewQuadric();
+	 btm=gluNewQuadric();
+	top=gluNewQuadric();
+	bulb=gluNewQuadric();
+	cube1=gluNewQuadric();
+	cube2=gluNewQuadric();
 
 }
 
